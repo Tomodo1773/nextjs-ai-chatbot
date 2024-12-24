50ab28f475160b592995bcbc36e58249d3e113a4
Tomodo1773 <forever-career@hotmail.co.jp>
2024-12-24 13:10:36 +0900

    デフォルトモデル名を「gemini-2.0-flash-exp」に変更

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -67,4 +67,4 @@ export const models: Array<Model> = [
 	},
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = "gpt-4o-mini";
+export const DEFAULT_MODEL_NAME: string = "gemini-2.0-flash-exp";
