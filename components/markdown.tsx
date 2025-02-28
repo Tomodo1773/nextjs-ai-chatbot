080c2f600d2f158fc1db0a49e0b1dc32d1b20035
Tomodo1773 <forever-career@hotmail.co.jp>
2025-02-28 17:02:16 +0900

    マークダウンコンポーネントのスタイルを更新し、テキストカラーを明示的に設定

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -12,13 +12,13 @@ const NonMemoizedMarkdown = ({ children }: { children: string }) => {
         // @ts-expect-error
         <pre
           {...props}
-          className={`${className} text-sm w-[80dvw] md:max-w-[500px] overflow-x-scroll bg-zinc-100 p-3 rounded-lg mt-2 dark:bg-zinc-800`}
+          className={`${className} text-sm w-[80dvw] md:max-w-[500px] overflow-x-scroll bg-zinc-100 text-zinc-900 p-3 rounded-lg mt-2 dark:bg-zinc-800 dark:text-white`}
         >
           <code className={match[1]}>{children}</code>
         </pre>
       ) : (
         <code
-          className={`${className} text-sm bg-zinc-100 dark:bg-zinc-800 py-0.5 px-1 rounded-md`}
+          className={`${className} text-sm bg-zinc-100 text-zinc-900 dark:bg-zinc-800 dark:text-white py-0.5 px-1 rounded-md`}
           {...props}
         >
           {children}
